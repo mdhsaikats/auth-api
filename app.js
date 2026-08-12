0468f573984692c99b97d377235e19e282dedbeb
mdhsaikats <saikatsikder2911@gmail.com>
2026-08-13 04:45:39 +0600

    need to work on the auth error on profile

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ import express from 'express';
 import authRouter from './src/routes/authRoutes.js';
 import userRouter from './src/routes/userRoutes.js';
 import morgan from 'morgan';
+
 const app = express();
+const appRouter = express.Router();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-//routes
-app.use("api/v1/auth", authRouter);
-app.use("api/v1/users", userRouter);
+// Routes
+appRouter.use('/api/v1/auth', authRouter);
+appRouter.use('/api/v1/users', userRouter);
 
-//middlewares
-//error handlers
+app.use(appRouter);
+
+// Error handlers
 
 export default app;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 import userName from '../handler/userHandler.js';
 router.get("/profile", userName);
-
+//TODO: need to work on the Authorization error
 export default router;
