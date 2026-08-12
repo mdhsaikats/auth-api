7fcec55701f1c6d4c199b52bd0e1c65f763d63b0
mdhsaikats <saikatsikder2911@gmail.com>
2026-08-12 23:39:14 +0600

    added the sql file

---
+++ authdb.sql
@@ -0,0 +1,15 @@
+﻿-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+
+DROP TABLE IF EXISTS "public"."users";
+-- Table Definition
+CREATE TABLE "public"."users" (
+    "id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    "name" varchar(255),
+    "email" varchar(255),
+    "password_hash" text,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("id")
+);
+
+
